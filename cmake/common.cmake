53dfecf64a156447af41d7ee454450e8780f3641
Takayama Fumihiko <tekezo@pqrs.org>
2025-05-18 18:40:55 +0900

    Update package-lock.cmake

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,22 +1,12 @@
 set(CPM_SOURCE_CACHE "${CMAKE_CURRENT_LIST_DIR}/tmp/cpm/source_cache")
 
-add_custom_target(vendor_headers ALL)
-
-function(copy_vendor_headers SRC DIR_DEST_SUB)
-  if(DIR_DEST_SUB STREQUAL "")
-    set(_dst "${VENDOR_INCLUDE_DIR}")
-  else()
-    set(_dst "${VENDOR_INCLUDE_DIR}/${DIR_DEST_SUB}")
-  endif()
-
-  add_custom_command(
-    TARGET vendor_headers PRE_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_directory "${SRC}" "${_dst}"
-    COMMENT "Vendoring: ${SRC} -> ${_dst}"
-  )
-endfunction()
+add_custom_target(vendor_files ALL)
 
 function(copy_vendor_package PKG)
+  #
+  # Check if it has already been copied
+  #
+
   get_property(done GLOBAL PROPERTY _COPIED_PACKAGES)
   list(FIND done "${PKG}" _idx)
   if(_idx GREATER -1)
@@ -25,21 +15,44 @@ function(copy_vendor_package PKG)
   list(APPEND done "${PKG}")
   set_property(GLOBAL PROPERTY _COPIED_PACKAGES "${done}")
 
-  if(DEFINED _HDR_SUB_${PKG})
-    set(src_sub "${_HDR_SUB_${PKG}}")
-  else()
-    set(src_sub "include")
-  endif()
-
-  if(DEFINED _DST_SUB_${PKG})
-    set(dst_sub "${_DST_SUB_${PKG}}")
-  else()
-    set(dst_sub "")
-  endif()
+  #
+  # git clone
+  #
 
   CPMAddPackage(NAME ${PKG})
 
-  copy_vendor_headers("${${PKG}_SOURCE_DIR}/${src_sub}" "${dst_sub}")
+  #
+  # Copy files
+  #
+
+  if(DEFINED _FILES_${PKG})
+    set(_files ${_FILES_${PKG}})
+  else()
+    set(_files "include -> /")
+  endif()
+
+  foreach(_file IN LISTS _files)
+    string(REPLACE "->" ";" _pair ${_file})
+    list(TRANSFORM _pair STRIP)
+    list(GET _pair 0 _src)
+    list(GET _pair 1 _dst)
+
+    set(_src "${${PKG}_SOURCE_DIR}/${_src}")
+    set(_dst "${VENDOR_INCLUDE_DIR}/${_dst}")
+
+    cmake_path(GET _dst PARENT_PATH _dst_dir)
+    file(MAKE_DIRECTORY ${_dst_dir})
+
+    add_custom_command(
+      TARGET vendor_files PRE_BUILD
+      COMMENT "Vendoring ${PKG}: \n  src: ${_src}\n  dst: ${_dst}"
+      COMMAND cp -r "${_src}" "${_dst}"
+    )
+  endforeach()
+
+  #
+  # Handle dependencies
+  #
 
   if(DEFINED _DEPS_${PKG})
     foreach(dep IN LISTS _DEPS_${PKG})
--- scripts/format-package-lock-cmake.ts
+++ scripts/format-package-lock-cmake.ts
@@ -17,16 +17,17 @@ const rows = [
     name: "",
     repo: "",
     tag: "",
+    downloadOnly: "",
   },
 ];
 const pattern =
-  /^CPMDeclarePackage\(\s*(\S+)\s+NAME\s+(\S+)\s+GITHUB_REPOSITORY\s+(\S+)\s+GIT_TAG\s+(\S+)\s*\)$/;
+  /^CPMDeclarePackage\(\s*(\S+)\s+NAME\s+(\S+)\s+GITHUB_REPOSITORY\s+(\S+)\s+GIT_TAG\s+(\S+)\s+DOWNLOAD_ONLY\s+(\S+)\s*\)$/;
 
 for (const l of lines) {
   const m = l.match(pattern);
   if (m) {
-    const [, alias, name, repo, tag] = m;
-    rows.push({ alias, name, repo, tag });
+    const [, alias, name, repo, tag, downloadOnly] = m;
+    rows.push({ alias, name, repo, tag, downloadOnly });
   }
 }
 
@@ -35,18 +36,21 @@ const widths = {
   name: Math.max(...rows.map((r) => r.name.length)),
   repo: Math.max(...rows.map((r) => r.repo.length)),
   tag: Math.max(...rows.map((r) => r.tag.length)),
+  downloadOnly: Math.max(...rows.map((r) => r.downloadOnly.length)),
 };
 
 let formattedText = "";
 for (const l of lines) {
   const m = l.match(pattern);
   if (m) {
-    const [, alias, name, repo, tag] = m;
+    const [, alias, name, repo, tag, downloadOnly] = m;
     formattedText +=
       `CPMDeclarePackage(${alias.padEnd(widths.alias)}` +
       `    NAME ${name.padEnd(widths.name)}` +
       `    GITHUB_REPOSITORY ${repo.padEnd(widths.repo)}` +
-      `    GIT_TAG ${tag})\n`;
+      `    GIT_TAG ${tag.padEnd(widths.tag)}` +
+      `    DOWNLOAD_ONLY ${downloadOnly}` +
+      `)\n`;
   } else {
     formattedText += `${l}\n`;
   }
