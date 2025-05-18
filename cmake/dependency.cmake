46f6e6aa6f3dca468c0759a894093e2a450af33d
Takayama Fumihiko <tekezo@pqrs.org>
2025-05-18 22:13:36 +0900

    Update dependency.cmake

--- cmake/dependency.cmake
+++ cmake/dependency.cmake
@@ -1,5 +1,8 @@
 set(_DEPS_type_safe "debug_assert")
-set(_DEPS_pqrs_cf_array "pqrs_cf_cf_ptr")
+set(_DEPS_pqrs_cf_array
+  "pqrs_cf_cf_ptr"
+  "pqrs_cf_string"
+)
 set(_DEPS_pqrs_cf_dictionary "pqrs_cf_cf_ptr")
 set(_DEPS_pqrs_cf_json
   "pqrs_cf_array"
